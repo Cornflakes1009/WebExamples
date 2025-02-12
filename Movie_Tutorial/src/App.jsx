f1a538945cea0b9027ef9f1f77025f0382b0bfef
Harold Davidson <harold.davidson89@gmail.com>
2025-02-11 22:02:57 -0800

    Add navbar and navigation to movie tutorial

--- Movie_Tutorial/src/App.jsx
+++ Movie_Tutorial/src/App.jsx
@@ -4,22 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { MovieCard } from './Components/MovieCard'
 import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import Favorite from './pages/Favorites'
+import NavBar from './Components/NavBar'
+
+
+// this defines a route. when going to /, we show the Home component.
+// <Routes> 
+//    <Route path='/' element={<Home />} />
+//    </Routes>
 
 function App() {
   const [count, setCount] = useState(0);
   //const displayMovie = 2;
   return (
     <>
-    <Home />
-      {/* simple ternary way of conditional rendering.  */}
-      {/* { displayMovie === 1 ? 
+      <div>
+        <NavBar />
+        <main className='main-content'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path="/favorites" element={<Favorite />} />
+          </Routes>
+        </main>
+        {/* simple ternary way of conditional rendering.  */}
+        {/* { displayMovie === 1 ? 
         <MovieCard movie={{title: "First Film", release_date: "2024"}} />  :
         <MovieCard movie={{title: "Second Film", release_date: "2024"}} />     
       }  */}
 
-      {/* short circuit way of conditional rendering. Second half is always true because the component does exist. */}
-      {/* { displayMovie === 2 && <MovieCard movie={{title: "Second Film", release_date: "2024"}} />    } */}
-       
+        {/* short circuit way of conditional rendering. Second half is always true because the component does exist. */}
+        {/* { displayMovie === 2 && <MovieCard movie={{title: "Second Film", release_date: "2024"}} />    } */}
+      </div>
+
+
     </>
   )
 }
--- Movie_Tutorial/src/pages/Home.jsx
+++ Movie_Tutorial/src/pages/Home.jsx
@@ -1,22 +1,34 @@
+import { useState } from "react"
 import { MovieCard } from "../Components/MovieCard"
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState(""); // searchQuery is the state and setSearchQuery is the function used to update state. When state changes, the entire component is re-rendered. You MUST use state to keep the values. 
+
     const movies = [
-        {id: 1, title: "John Wick", release_date: "2020"},
-        {id: 2, title: "Terminator", release_date: "2020"},
-        {id: 3, title: "The Matrix", release_date: "2020"}
+        { id: 1, title: "John Wick", release_date: "2020" },
+        { id: 2, title: "Terminator", release_date: "2020" },
+        { id: 3, title: "The Matrix", release_date: "2020" }
     ]
 
-    const handleSearch = () => {
 
-    }
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery("");
+    };
+
     return <div className="movies">
-        <form onSubmit={handleSearch} className="search-form"> 
-            <input type="text" placeholder="Search for movies" className="search-inpu" />
+        <form onSubmit={handleSearch} className="search-form">
+            <input type="text" placeholder="Search for movies" className="search-inpu" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
             <button type="submit" className="search-button">Search</button>
         </form>
         <div className="movies-grid">
-            { movies.map( movie => <MovieCard movie={movie} key={movie.id }/> )}
+            {movies.map((movie) =>
+            (
+                <MovieCard movie={movie} key={movie.id} />
+            )
+            )}
         </div>
     </div>
 }
